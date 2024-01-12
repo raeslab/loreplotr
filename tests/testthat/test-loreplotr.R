adc19db591e90bc2965fed3f3fe68be279b467ae
u0123248 <sebastian.proost@kuleuven.vib.be>
2024-01-12 11:46:35 +0100

    added tests and github action

---
+++ tests/testthat/test-loreplotr.R
@@ -0,0 +1,10 @@
+
+test_that("loreplotr works", {
+  data("mtcars")
+  mtcars$cyl <- paste("cyl", mtcars$cyl, sep="_")
+
+  expect_no_error(mtcars %>% loreplotr("mpg", "cyl"))
+  expect_error(mtcars %>% loreplotr("mpg", "unknowncol")) # One column doesn't exist
+  expect_error(mtcars %>% loreplotr("unknowncol", "cyl")) # Other column doesn't exist
+  expect_error(mtcars %>% loreplotr("xyz", "unknowncol")) # Both columns doesn't exist
+})
